ba0ab4c67fd437a0acd8b69aa91bb76a1116374c
LukasBudach <lukas.budach@student.hpi.de>
2020-06-17 22:18:24 +0200

    add commandline arguments to train_enas, provide defaults as config file

--- train_enas.py
+++ train_enas.py
@@ -5,6 +5,12 @@ from mxnet import nd
 from autogluon.utils import DataLoader
 from visualization.color_graphs import format_and_render
 from pathlib import Path
+try:
+    from persistentargparse import PersistentArgumentParser
+except ImportError:
+    print('Could not import from persistentargparse. The module should be added as git submodule to this repository. '
+          'Please run git submodule init and git submodule update --remote')
+    exit(1)
 
 def create_mock_gluon_image_dataset(num_samples=10, img_width=32, img_height=32, num_channels=3, num_classes=10):
     X = nd.random.uniform(shape=(num_samples,num_channels,img_height,img_width))
@@ -42,14 +48,26 @@ def train_net_enas(net, epochs, name, log_dir='./logs/',
     scheduler.run()
 
 
-def main():
-    train_set = 'cifar100'
-    val_set = None
+def main(args):
+    train_set = args.train_data
+    val_set = args.val_data
     #train_set, val_set = create_mock_gluon_image_dataset()
     #we set num_gpus=(1,) because when specifying a tuple we can set a specific gpu mapping
-    train_net_enas(meliusnet22_enas().enas_sequential, 100, 'meliusnet22_enas_kernelsize1', train_set=train_set, val_set=val_set, batch_size=128, num_gpus=(0,))
+    train_net_enas(meliusnet22_enas().enas_sequential, args.epochs, 'meliusnet22_enas_kernelsize1', train_set=train_set,
+                   val_set=val_set, batch_size=args.batch_size, num_gpus=(args.num_gpus,))
     #train_net_enas(meliusnet59_enas().enas_sequential, 3, 'meliusnet59', train_set=train_set, val_set=val_set, batch_size=5)
 
 
 if __name__=="__main__":
-    main()
+    parser = PersistentArgumentParser(description='Train ENAS is the script provided to train the defined ENAS network '
+                                                  'in order to find a different architecture for the net.')
+
+    parser.add_argument('-b', '--batch-size', type=int, required=True, help='Batch size to use during training.')
+    parser.add_argument('-e', '--epochs', type=int, required=True, help='Number of epochs to train for.')
+    parser.add_argument('--num-gpus', type=int, required=True, help='Number of available GPUs to use for the training.')
+    parser.add_argument('--train-data', type=str, required=True, help='Autogluon specifier for the dataset to use for '
+                                                                      'training.')
+    parser.add_argument('--val-data', type=str, required=False, help='Autogluon specifier for the dataset to use for '
+                                                                     'validation.')
+
+    main(parser.parse_args())
