9ed2defb0b243e7f0ff69990709df7006c23622b
Padl20t4 <padl20t4@fake.mail.com>
2020-07-28 12:26:53 +0200

    Fix bugs in blocks_summar.py

--- utils/blocks_summary.py
+++ utils/blocks_summary.py
@@ -6,6 +6,10 @@ def main():
     for folder in os.listdir(training_dir):
         graph_dir = training_dir + folder + '/logs/architectures/'
         summary_table = []
+
+        if (not os.path.isdir(graph_dir)) or ('meliusnet' not in str(folder)):
+            continue
+	
         for txt_filepath in sorted([x for x in os.listdir(graph_dir) if x.endswith('txt')]):
 
             full_path = graph_dir + txt_filepath
@@ -18,7 +22,7 @@ def main():
             for i in range(len(architecture)):
                 if architecture[i:i + 14] == 'DenseBlockEnas':
                     type_of_convolution = architecture[i + 314:i + 352]
-                    if type_of_convolution.find('(1, 1)') > -1:
+                    if type_of_convolution.find('(1, 1)') == -1:
                         num_used_dense_blocks += 1
 
             row = (txt_filepath.split('.txt')[0], num_used_dense_blocks, num_used_improvement_blocks)
